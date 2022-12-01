f25474ea239a4e49cfd5985e63c557304e294265
PhilSoGooood <peelhw@gmail.com>
2022-12-01 23:54:28 +0900

    :tada: 프로젝트 초기 설정

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from '@/routes/index';
+import store from '@/store/index';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import VueCookies from 'vue-cookies';
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(VueCookies, { expires: '7d' });
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.config.productionTip = false;
+new Vue({
+  render: h => h(App),
+  router,
+  store,
+}).$mount('#app');
