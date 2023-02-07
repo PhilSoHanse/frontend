8ef16d0a82d19ad87c3ad5ec2539e5d7d543b04e
PhilSoGooood <peelhw@gmail.com>
2023-02-07 17:53:19 +0900

    :ambulance: vue proxy 적용

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,11 @@ module.exports = defineConfig({
     client: {
       overlay: false,
     },
+    proxy: {
+      '/': {
+        target: process.env.VUE_APP_API_URL,
+        changeOrigin: true,
+      },
+    },
   },
 });
